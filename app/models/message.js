b064cd7a308022807881eff4b427fe42d14869da
Andy Jiang <andyjiang@gmail.com>
2013-12-02 21:50:22 -0800

    added message.js

---
+++ app/models/message.js
@@ -0,0 +1,50 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/**
+ * Message Schema
+ */
+var MessageSchema = new Schema({
+    created_on: {
+        type: Date,
+        default: Date.now
+    },
+    body: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    to: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    from: {
+        type: String,
+        default: '',
+        trim: true
+    }
+})
+
+/**
+ * Validations
+ */
+// ArticleSchema.path('title').validate(function(title) {
+//     return title.length;
+// }, 'Title cannot be blank');
+
+/**
+ * Statics
+ */
+MessageSchema.statics = {
+    load: function(id, cb) {
+        this.findOne({
+            _id: id
+        }).populate('body', 'body').exec(cb);
+    }
+};
+
+mongoose.model('Article', ArticleSchema);
