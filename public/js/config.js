a6d18d2d9eadec754dfcd8b3481551b18b36b732
Andy Jiang <andyjiang@gmail.com>
2013-12-08 15:39:31 -0800

    moved config to js folder

--- public/js/config.js
+++ public/js/config.js
@@ -1,11 +1,9 @@
-// public/core.js
 var chatView = angular.module('chatView', []);
 
 function mainController($scope, $http) {
-    $scope.formData = {};
-
     // When landing on the page, get all chats and a few pieces of each chat history.
     // You need to access the Nodejs API here.
+    // Load only the Messages where your number is either the 'to' or the 'from'.
     $http.get('/api/messages')
         .success(function(data) {
             $scope.messages = data;
@@ -13,14 +11,27 @@ function mainController($scope, $http) {
         })
         .error(function(data) {
             console.log('Error: ' + data);
+        });
+
+    // Is it possible to call a function here to update messages.
+    $http.post('/api/messages')
+        .success(function(data) {
+            $scope.messages = data;
+            console.log(data);
         })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
 
     // When submitting a chat message, send the text to the node API
     // Node API will then send call function to send it via Twilio
     // and to save it to MongoDB.
     $scope.sendMessage = function() {
         // Make sure $scope.formData has the 'to' field.
-        $http.post('/api/message', $scope.formData)
+        var postLoad = {};
+        postLoad.formData = $scope.formData;
+        postLoad.toData = $scope.toData;
+        $http.post('/api/message', postLoad)
             .success(function(data) {
                 $('input').val('');
                 $scope.messages = data;
